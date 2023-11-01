3c4219d19d191da9aac6ef590f1297aa285788e2
KasunUmesh <kasunumesh38@gmail.com>
2023-11-02 04:54:41 +0530

    init Project

---
+++ src/main/java/lk/ijse/Travel_Planning_System/GuideServices/GuideServicesApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Travel_Planning_System.GuideServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideServicesApplication.class, args);
+	}
+
+}
