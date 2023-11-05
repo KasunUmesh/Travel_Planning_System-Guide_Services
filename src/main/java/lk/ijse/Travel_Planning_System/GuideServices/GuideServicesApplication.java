0c6f66c1ab8f411a6b0a047136223a61a8a8e44d
KasunUmesh <kasunumesh38@gmail.com>
2023-11-05 05:35:39 +0530

    created exception, WebAppConfig and ResponseUtil class

--- src/main/java/lk/ijse/Travel_Planning_System/GuideServices/GuideServicesApplication.java
+++ src/main/java/lk/ijse/Travel_Planning_System/GuideServices/GuideServicesApplication.java
@@ -2,8 +2,9 @@ package lk.ijse.Travel_Planning_System.GuideServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GuideServicesApplication {
 
 	public static void main(String[] args) {
